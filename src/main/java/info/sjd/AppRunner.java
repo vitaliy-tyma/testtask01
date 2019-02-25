500e8b35fa26350e3733f54135fcfae10382439f
Tymashkov <VTymashkov@kiev.luxoft.com>
2019-02-25 18:01:57 +0200

    Initial commit

---
+++ src/main/java/info/sjd/AppRunner.java
@@ -0,0 +1,10 @@
+package info.sjd;
+
+public class AppRunner {
+
+    public static void main(String[] argv) {
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        System.out.println("Hello world");
+        System.out.println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
+    }
+}
